ec28c722c6cc0fbb0a4481d0142f230610d10b2d
David Schwarz <schwarz.david10@gmail.com>
2019-04-14 18:00:19 +0200

    inliningCss ... margin needs fix

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const BootstrapEmail = require('./lib/BootstrapEmail');
 const bootstrapEmail = new BootstrapEmail({
 	template: path.resolve('./tests/preinlined/lyft.html')
 });
-bootstrapEmail.compile();
+bootstrapEmail.compileAndSave(path.join(__dirname, 'output.html'));
