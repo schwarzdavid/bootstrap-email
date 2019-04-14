928e357c9c1dbd532ab57a83529ca990cb3c5aaf
David Schwarz <schwarz.david10@gmail.com>
2019-04-14 19:12:43 +0200

    Ruby libs removed, spacing fixed, html attributes inject added

--- ruby/bootstrap-email/engine.rb
+++
@@ -1,6 +0,0 @@
-module BootstrapEmail
-  module Rails
-    class Engine < ::Rails::Engine
-    end
-  end
-end
--- ruby/bootstrap-email/version.rb
+++
@@ -1,7 +0,0 @@
-module BootstrapEmail
-  module Rails
-    VERSION = File.read(
-      File.expand_path('../../VERSION', __dir__)
-    ).strip
-  end
-end
