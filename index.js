afc17c297701712a672e16e95bb6f8fa9a154408
David Schwarz <schwarz.david10@gmail.com>
2019-04-15 15:56:05 +0200

    Compile order updated & tested

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const path = require('path');
 const BootstrapEmail = require('./lib/BootstrapEmail');
 
 const bootstrapEmail = new BootstrapEmail({
-	template: path.resolve('./tests/preinlined/custom.html')
+	template: path.resolve('./tests/preinlined/lyft.html')
 });
 bootstrapEmail.compileAndSave(path.join(__dirname, 'output.html'));
