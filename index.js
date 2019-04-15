75f791a61a247f5174e66a8955dfe80f0f107a82
David Schwarz <schwarz.david10@gmail.com>
2019-04-15 18:25:16 +0200

    Should be stable now

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const path = require('path');
 const BootstrapEmail = require('./lib/BootstrapEmail');
+const fs = require('fs');
+
+const inputPath = path.join(__dirname, 'tests/input');
+const dir = fs.readdirSync(inputPath);
+const files = dir.map(filename => {
+	return path.join(inputPath, filename);
+});
 
 const bootstrapEmail = new BootstrapEmail({
-	template: path.resolve('./tests/preinlined/lyft.html')
+	templates: files
 });
-bootstrapEmail.compileAndSave(path.join(__dirname, 'output.html'));
+bootstrapEmail.compileAndSave(path.join(__dirname, 'tests/output'));
